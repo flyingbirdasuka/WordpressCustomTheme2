86f2fad21816279f1c866d31943eb367b2d6760f
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2021-11-20 17:31:53 +0100

    tommu changed

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
     <section class="search">
         <div class="container" data-aos="fade-up">
-           <h1>Page not found! hallooooo</h1>
+           <h1>Page not found! 0o0o0o0o0o</h1>
 		        <?php
 		        get_search_form();
 		        ?>
