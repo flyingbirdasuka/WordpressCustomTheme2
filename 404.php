6bc9170d7a1aab0161895d38405a0ca21253dcc1
Asuka <72805418+flyingbirdasuka@users.noreply.github.com>
2022-07-06 05:38:01 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
-
-    <section class="search">
+	<section class="search">
         <div class="container" data-aos="fade-up">
-           <h1>Page not found! :D 0o0o0o0o0o</h1>
+           <h1>Page not found</h1>
 		        <?php
 		        get_search_form();
 		        ?>
