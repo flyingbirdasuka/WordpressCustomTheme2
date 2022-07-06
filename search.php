3b2c3eda5ffef59c233d40d620afa92cbe22d08a
Asuka <72805418+flyingbirdasuka@users.noreply.github.com>
2022-07-06 05:49:42 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,25 +1,8 @@
-
 <?php if ( have_posts() ) : ?>
-
+	<?php
+	get_template_part( 'template-parts/content','search');
 	
-
-	<?php /* Start the Loop */ ?>
-	<?php //while ( have_posts() ) : the_post(); ?>
-
-		<?php
-		/**
-		 * Run the loop for the search to output the results.
-		 * If you want to overload this in a child theme then include a file
-		 * called content-search.php and that will be used instead.
-		 */
-		get_template_part( 'template-parts/content','search');
-		
-		?>
-
-	<?php //endwhile; ?>
-
-	
-
+	?>
 <?php else : ?>
 
 	<?php get_template_part( 'template-parts/content', 'none' ); ?>
